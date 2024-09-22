2e9f8b12b215f1db2a71da4c079a7679df216a22
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-09-22 13:43:12 +0200

    may be the end

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -25,24 +25,22 @@ export const renderImages = images => {
         comments,
         downloads,
       }) => `
-      <ul>
-        <li class="gallery-item">
-            <a href="${largeImageURL}" class="gallery-link">
-                <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
-            </a>
-            <div class="info">
-                <p class="info-item"><span>Likes</span> ${likes}</p>
-                <p class="info-item"><span>Views</span> ${views}</p>
-                <p class="info-item"><span>Comments</span> ${comments}</p>
-                <p class="info-item"><span>Downloads</span> ${downloads}</p>
-            </div>
-        </li>
-        </ul>
-    `
+            <li class="gallery-item">
+                <a href="${largeImageURL}" class="gallery-link">
+                    <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+                </a>
+                <div class="info">
+                    <p class="info-item"><span>Likes:</span> ${likes}</p>
+                    <p class="info-item"><span>Views:</span> ${views}</p>
+                    <p class="info-item"><span>Comments:</span> ${comments}</p>
+                    <p class="info-item"><span>Downloads:</span> ${downloads}</p>
+                </div>
+            </li>
+        `
     )
     .join('');
 
-  gallery.innerHTML = `<div>${markup}</div>`;
+  gallery.innerHTML = markup;
 };
 
 export const showLoader = () => {
